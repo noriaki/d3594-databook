d0ebd82def6cf0f92f9570bc0bdec86f7e3384cd
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-09-10 13:41:35 +0900

    change identify algorithm

--- export.js
+++ export.js
@@ -1,7 +1,27 @@
 const { readdirSync, readFileSync, writeFileSync } = require('fs');
 const { resolve } = require('path');
+const { createHash } = require('crypto');
 const moment = require('moment');
 const stringify = require('csv-stringify/lib/sync');
+
+const main = () => {
+  const basedirname = 'data';
+  const filenames = readdirSync(resolve(basedirname));
+  const correctFilename = /^\d+\.json$/;
+  const commanders = [];
+  const tactics = [];
+  for (const filename of filenames) {
+    if (!correctFilename.test(filename)) { continue; }
+    const filepath = resolve(basedirname, filename);
+    const data = JSON.parse(readFileSync(filepath));
+    data.crawledAt = moment(data.crawledAt).format('YYYY/MM/DD');
+    shapeCommander(data);
+    commanders.push(flatten(data));
+  }
+  const columns = Object.keys(commanders[0]);
+  const options = { quoted: true, header: true, columns };
+  // writeFileSync('./output/output.csv', stringify(commanders, options));
+};
 
 /**
  * PRIVATE
@@ -25,19 +45,32 @@ const flatten = object => (
   })(object))
 );
 
-(() => {
-  const basedirname = 'data';
-  const filenames = readdirSync(resolve(basedirname));
-  const correctFilename = /^\d+\.json$/;
-  const commanders = [];
-  for (const filename of filenames) {
-    if (!correctFilename.test(filename)) { continue; }
-    const filepath = resolve(basedirname, filename);
-    const data = flatten(JSON.parse(readFileSync(filepath)));
-    data.crawledAt = moment(data.crawledAt).format('YYYY/MM/DD');
-    commanders.push(data);
-  }
-  const columns = Object.keys(commanders[0]);
-  const options = { quoted: true, header: true, columns };
-  writeFileSync('./output/output.csv', stringify(commanders, options));
-})();
+const shapeCommander = (data) => {
+  data.army = formattedArmy(data.army);
+  data.id = humanizeId(data);
+  const identifier = identify(data);
+  console.log(`${identifier}:${data.id}`);
+};
+
+const shapeTactics = (data) => {};
+
+const formattedArmy = (army) => army.replace('\u5175', '');
+
+const humanizeId = ({ name, rarity, special, team, army }) => {
+  const nameSuffix = special !== null ? `(${special})` : '';
+  return `\u2605${rarity}\u30FB${name}${nameSuffix}\u30FB${team}\u30FB${army}`;
+};
+
+const identify = (identifier) => (
+  typeof identifier === 'string' ?
+    md5(identifier) : md5(humanizeId(identifier))
+);
+
+const md5 = (src) => {
+  const hash = createHash('md5');
+  hash.update(src, 'binary');
+  return hash.digest('hex');
+};
+
+
+main();
