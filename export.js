bf89131014d7acffba95de1f2c5bee106bc6b2fc
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-09-11 15:35:21 +0900

    refactor export methods

--- export.js
+++ export.js
@@ -6,26 +6,46 @@ const stringify = require('csv-stringify/lib/sync');
 const {
   humanizeId,
   identify,
+  md5,
 } = require('./libs/identify');
 
 const main = () => {
   const basedirname = 'data';
   const filenames = readdirSync(resolve(basedirname));
-  const correctFilename = /^\d+\.json$/;
+  const correctFilename = /^[0-9a-f]+\.json$/;
   const commanders = [];
-  const tactics = [];
+  const specificTacticsList = [];
+  const analyzablesTacticsList = [];
   for (const filename of filenames) {
     if (!correctFilename.test(filename)) { continue; }
     const filepath = resolve(basedirname, filename);
     const data = JSON.parse(readFileSync(filepath));
     data.crawledAt = moment(data.crawledAt).format('YYYY/MM/DD');
-    shapeCommander(data);
-    commanders.push(flatten(data));
+    const {
+      commander,
+      specificTactics,
+      analyzablesTactics,
+    } = shapeData(data);
+    commanders.push(flatten(commander));
+    if (specificTactics != null) {
+      specificTacticsList.push(flatten(specificTactics));
+    }
+    analyzablesTactics.forEach((tactics) => {
+      if (analyzablesTacticsList.every(notEqual(tactics))) {
+        analyzablesTacticsList.push(flatten(tactics));
+      }
+    });
   }
-  const columns = Object.keys(commanders[0]);
-  const options = { quoted: true, header: true, columns };
-  // writeFileSync('./output/output.csv', stringify(commanders, options));
+  writeCsvFile('./output/commanders.csv', commanders);
+  writeCsvFile('./output/specificTactics.csv', specificTacticsList);
+  writeCsvFile('./output/analyzablesTactics.csv', analyzablesTacticsList);
 };
+
+const writeCsvFile = (filepath, data) => {
+  const columns = Object.keys(data[0]);
+  const options = { quoted: true, header: true, columns };
+  writeFileSync(filepath, stringify(data, options));
+}
 
 /**
  * PRIVATE
@@ -49,15 +69,48 @@ const flatten = object => (
   })(object))
 );
 
-const shapeCommander = (data) => {
-  data.army = formattedArmy(data.army);
-  data.id = humanizeId(data);
-  const identifier = identify(data);
-  console.log(`${identifier}:${data.id}`);
+const notEqual = ({ identifier }) => tactics => identifier !== tactics.identifier;
+
+const shapeData = (data) => {
+  const { tactics, ...commander } = data;
+  const finalCommander = shapeCommander(commander);
+  let specificTactics;
+  if (tactics.init != null) {
+    specificTactics = shapeTactics(tactics.init);
+    finalCommander.specificTacticsId = specificTactics.identifier;
+  }
+  const analyzablesTactics = tactics.analyzables.map(shapeTactics);
+  finalCommander.analyzableTacticsIds = fillArray(
+    2, analyzablesTactics.map(t => t.identifier)
+  );
+  return {
+    commander: finalCommander,
+    specificTactics,
+    analyzablesTactics,
+  };
 };
 
-const shapeTactics = (data) => {};
+const shapeCommander = (commander) => {
+  commander.army = formattedArmy(commander.army);
+  commander.id = humanizeId(commander);
+  commander.identifier = identify(commander);
+  delete commander.url;
+  delete commander.imageUrl;
+  return commander;
+};
+
+const shapeTactics = (tactics) => {
+  tactics.identifier = md5(tactics.name);
+  tactics.permissions = fillArray(3, tactics.permissions);
+  delete tactics.id;
+  delete tactics.url;
+  return tactics;
+};
 
 const formattedArmy = (army) => army.replace('\u5175', '');
+
+const fillArray = (maxSize, fillerArray, defaultValue = null) => (
+  Array.from(new Array(maxSize), (_, i) => fillerArray[i] || defaultValue)
+);
 
 main();
