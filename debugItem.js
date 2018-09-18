8a665598dc054eba89cf4a681ec22c0829acd71f
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-09-18 21:54:18 +0900

    update debug info

--- debugItem.js
+++ debugItem.js
@@ -6,6 +6,10 @@ const {
   visitItemPage,
   extractCommanderData,
 } = require('./libs/crawl');
+const {
+  identify,
+  humanizeId,
+} = require('./libs/identify');
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -15,7 +19,7 @@ const {
   const id = '19980';
   const itemPage = await visitItemPage(page, id);
   const data = await extractCommanderData(itemPage);
-  console.log(`${data.id}: ${data.name}`);
+  console.log(`${data.id}:${humanizeId(data)}: ${data.name} (${identify(data)})`);
   console.log(data, data.tactics); // debug
 })().then(() => process.exit()).catch(logAndExit);
 
--- debugItem2.js
+++ debugItem2.js
@@ -6,16 +6,20 @@ const {
   visitItemPage,
   extractCommanderData,
 } = require('./libs/crawl2');
+const {
+  identify,
+  humanizeId,
+} = require('./libs/identify');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.emulate(devices['iPhone 7']);
 
-  const id = '2295';
+  const id = '8540';
   const itemPage = await visitItemPage(page, id);
   const data = await extractCommanderData(itemPage);
-  console.log(`${data.id}: ${data.name}`);
+  console.log(`${data.id}:${humanizeId(data)}: ${data.name} (${identify(data)})`);
   console.log(data, data.tactics, data.status); // debug
 })().then(() => process.exit()).catch(logAndExit);
 
