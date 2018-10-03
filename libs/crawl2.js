efdbbd87e0a6e4c78434778408e0b3985b231d60
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-10-03 19:15:39 +0900

    fix crawling pagination

--- libs/crawl2.js
+++ libs/crawl2.js
@@ -22,7 +22,7 @@ const getCommandersData = async (page) => {
   const idCount = ids.length;
   const windowSize = 10;
   const pageCount = Math.floor(idCount / windowSize);
-  for (let pageIndex = 0; pageIndex < pageCount; pageIndex++) {
+  for (let pageIndex = 0; pageIndex <= pageCount; pageIndex++) {
     console.group(`----- page ${pageIndex + 1} of ${pageCount}`);
     const offset = pageIndex * windowSize;
     const currentIds = ids.slice(offset, offset + windowSize);
