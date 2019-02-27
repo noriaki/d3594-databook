fd6897fc031336cd064fcabdc1ebcf6d33f0d499
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-02-27 19:50:47 +0900

    add decimalPlaces option, add viewBox attr in output svg

--- libs/TextToSVG.js
+++ libs/TextToSVG.js
@@ -1,6 +1,10 @@
+const { round } = require('lodash');
 const opentype = require('opentype.js');
 
-const defaultOptions = { fontSize: 40 };
+const defaultOptions = {
+  fontSize: 40,
+  decimalPlaces: 2,
+};
 
 class TextToSVG {
   constructor(font) {
@@ -12,46 +16,68 @@ class TextToSVG {
   }
 
   getWidth(text, options = defaultOptions) {
-    const { fontSize, ...otherOptions } = options;
-    return this.font.getAdvanceWidth(text, fontSize, otherOptions);
+    return this.getDimensions(text, options).width;
   }
 
-  getHeight(fontSize = defaultOptions.fontSize) {
-    const fontScale = 1 / this.font.unitsPerEm * fontSize;
-    return ((this.font.ascender - this.font.descender) * fontScale);
+  getHeight(text, options = defaultOptions) {
+    return this.getDimensions(text, options).height;
   }
 
   getGlyphs(text) {
     return this.font.stringToGlyphs(text);
   }
 
+  getDimensions(text, options = defaultOptions) {
+    const { decimalPlaces, ...others } = options;
+    const { x1, x2, y1, y2 } = this.getPath(text, others).getBoundingBox();
+    return {
+      x: round(x1, decimalPlaces),
+      y: round(y1, decimalPlaces),
+      width: round(x2 - x1, decimalPlaces),
+      height: round(y2 - y1, decimalPlaces),
+    };
+  }
+
   getPath(text, options = defaultOptions) {
-    const { fontSize, ...otherOptions } = options;
+    const { fontSize, decimalPlaces, ...otherOptions } = options;
     const fontScale = 1 / this.font.unitsPerEm * fontSize;
     // max char height from baseline
     const baselineHeight = Math.max(...this.getGlyphs(text).map((g) => {
       const { y1, y2 } = g.getBoundingBox();
-      return (y2 - y1) * fontScale;
+      return round((y2 - y1) * fontScale, decimalPlaces);
     }));
     return this.font.getPath(text, 0, baselineHeight, fontSize, otherOptions);
   }
 
   toPathData(text, options = defaultOptions) {
-    const path = this.getPath(text, options)
-    return path.toPathData();
+    const { decimalPlaces, ...otherOptions } = options;
+    const path = this.getPath(text, otherOptions);
+    return path.toPathData(decimalPlaces);
   }
 
   toPath(text, options = defaultOptions) {
-    const path = this.getPath(text, options)
-    return path.toSVG();
+    const { decimalPlaces, ...otherOptions } = options;
+    const path = this.getPath(text, otherOptions);
+    return path.toSVG(decimalPlaces);
   }
 
-  toSVG(text, options = defaultOptions) {
+  toSVG(text, id, options = defaultOptions) {
     const pathTag = this.toPath(text, options);
-    const startTag = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">';
+    const dimensions = this.getDimensions(text, options)
+    const startTag = generateSvgStartTag(id, dimensions);
     const endTag = '</svg>';
     return [startTag, pathTag, endTag].join('');
   }
 }
 
 module.exports = TextToSVG;
+
+const generateSvgStartTag = (id, { x, y, width, height }) => (
+  [
+    '<svg',
+    `id="${id}"`,
+    `viewBox="${x} ${y} ${width} ${height}"`,
+    'xmlns="http://www.w3.org/2000/svg"',
+    'xmlns:xlink="http://www.w3.org/1999/xlink">',
+  ].join(' ')
+);
